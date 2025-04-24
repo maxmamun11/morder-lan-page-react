a8ed7e662ca5d9e9ed84a86003d4ca4cab2506f2
Abdullah Al Mamun <abdullaalmamun100@gmail.com>
2025-04-24 10:20:59 +0800

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1>Hello Vite + React!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
